19dcaa3f7af661cc2a15b67421f07a726fcb3b25
ramajoballester <alvaroramajo1@gmail.com>
2023-09-20 18:54:00 +0200

    Remove evaluation source and create pypi project

--- uc3m_pic/utils.py
+++
@@ -1,9 +0,0 @@
-
-
-def conv2d_output_size(input_size, kernel_size, padding=0, stride=1,
-                          pool_kernel_size=1, pool_stride=1):
-    
-    conv_output = (input_size + 2*padding - kernel_size) // stride + 1
-    pool_output = (conv_output - pool_kernel_size) // pool_stride + 1
-
-    return pool_output
